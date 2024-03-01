e99c9711813f69b41d3b966898a855c7c992ae11
Bhupendra Pal <bhupendrapal0994@gmail.com>
2024-03-01 13:16:19 +0530

    debug errors

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@
 
 import Header from "./components/Header";
 import NewsBoard from "./components/Newsboard";
+import { TypewriterEffectSmoothDemo } from "./components/Typewritter";
 import { BackgroundGradientAnimation } from "./components/ui/background-gradient-animation";
 
 function App() {
   return (
     <BackgroundGradientAnimation>
       <div className="dark">
+        <TypewriterEffectSmoothDemo  />
         <Header />
+        <div >
+
         <NewsBoard />
+        </div>
       </div>
     </BackgroundGradientAnimation>
   );
---
+++ src/components/Typewritter.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { TypewriterEffectSmooth } from "./ui/typewriter-effect";
+export function TypewriterEffectSmoothDemo() {
+  const words = [
+    {
+      text: "Build",
+    },
+    {
+      text: "awesome",
+    },
+    {
+      text: "app",
+    },
+    {
+      text: "with",
+    },
+    {
+      text: "NEXT JS.",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+  ];
+  return (
+    <div className="flex flex-col items-center justify-center h-[40rem]  ">
+      <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
+       Hi, I'm Bhupendra Pal. This is a news-app project.
+      </p>
+      <TypewriterEffectSmooth words={words} />
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
+        
+      </div>
+    </div>
+  );
+}
